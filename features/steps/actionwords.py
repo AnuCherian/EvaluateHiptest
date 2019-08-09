554b739dd9712519756e5e374f042d115ece7b9c
AnuCherian <anucherian@gmail.com>
2019-08-09 15:24:20 +1200

    FC

---
+++ features/steps/actionwords.py
@@ -0,0 +1,14 @@
+# encoding: UTF-8
+
+class Actionwords:
+    def __init__(self, test):
+        self.test = test
+
+    def we_have_behave_installed(self):
+        pass
+
+    def we_implement_a_test(self):
+        pass
+
+    def trial_will_test_it_for_us(self):
+        pass
