9428c25558b76d20bc121897ab25795e5d32da3a
AnuCherian <anucherian@gmail.com>
2019-08-12 13:50:06 +1200

    Q1

--- features/steps/actionwords.py
+++ features/steps/actionwords.py
@@ -13,3 +13,21 @@ class Actionwords:
 
     def trial_will_test_it_for_us(self):
         pass
+
+    def the_ninja_has_a_third_level_blackbelt(self):
+        pass
+
+    def attacked_by_a_samurai(self):
+        pass
+
+    def hen_the_ninja_should_engage_the_opponent(self):
+        pass
+
+    def the_ninja_should_engage_the_opponent(self):
+        pass
+
+    def attacked_by_chuck_norris(self):
+        pass
+
+    def the_ninja_should_run_for_his_life(self):
+        pass
--- features/steps/test_project.py
+++ features/steps/test_project.py
@@ -13,3 +13,19 @@ class TestMyFirstTest(unittest.TestCase):
         self.actionwords.we_implement_a_test()
         # Then trial will test it for us!
         self.actionwords.trial_will_test_it_for_us()
+
+    def test_weaker_opponent(self):
+        # Given the ninja has a third level black-belt
+        self.actionwords.the_ninja_has_a_third_level_blackbelt()
+        # When attacked by a samurai
+        self.actionwords.attacked_by_a_samurai()
+        # Then the ninja should engage the opponent
+        self.actionwords.the_ninja_should_engage_the_opponent()
+
+    def test_stronger_opponent(self):
+        # Given the ninja has a third level black-belt
+        self.actionwords.the_ninja_has_a_third_level_blackbelt()
+        # When attacked by Chuck Norris
+        self.actionwords.attacked_by_chuck_norris()
+        # Then the ninja should run for his life
+        self.actionwords.the_ninja_should_run_for_his_life()
